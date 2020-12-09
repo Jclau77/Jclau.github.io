cae6caae56665d496ba218dfb9a863930b73e0c2
Jclau77 <jclau168@hotmail.com>
2020-12-08 22:22:22 -0500

    Final changes

--- contact.js
+++ contact.js
@@ -24,13 +24,35 @@ let issue = () => {
 };
 
 let comments = () => {
-  document.getElementById('inquiry').addEventListener('click', () => {
-    document.getElementById('ordercode').innerText = ' ';
+  document.getElementById('comment').addEventListener('click', () => {
+    let div = document.getElementById('userComment');
+    let label = document.createElement('label');
+    label.setAttribute('for', 'userComment');
+    let txt = document.createTextNode('What is your comment:');
+    label.appendChild(txt);
+    let userInput = document.createElement('input');
+    userInput.setAttribute('type', 'text');
+    userInput.setAttribute('name', 'comment');
+    userInput.setAttribute('placeholder', 'Enter your comment:');
+    userInput.required = true;
+    div.appendChild(label);
+    div.appendChild(userInput);
   });
 };
 
 let inquiry = () => {
-  document.getElementById('comment').addEventListener('click', () => {
-    document.getElementById('ordercode').innerText = ' ';
+  document.getElementById('question').addEventListener('click', () => {
+    let div = document.getElementById('userQuestion');
+    let label = document.createElement('label');
+    label.setAttribute('for', 'userQuestion');
+    let txt = document.createTextNode('What is your Question:');
+    label.appendChild(txt);
+    let userInput = document.createElement('input');
+    userInput.setAttribute('type', 'text');
+    userInput.setAttribute('name', 'question');
+    userInput.setAttribute('placeholder', 'Enter your Question:');
+    userInput.required = true;
+    div.appendChild(label);
+    div.appendChild(userInput);
   });
 };
